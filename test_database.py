26d21a16b22e21d13f0991f4c019cc7fd6366cd6
lfcourtney <lukechopper22@gmail.com>
2025-11-14 18:56:54 +0000

    Added 'TestStaff' class to test file

--- test_database.py
+++ test_database.py
@@ -1,5 +1,5 @@
 import pytest
-from challenge import StaffAnimal
+from challenge import StaffAnimal, Staff
 
 
 # Test the 'StaffAnimal' class
@@ -32,3 +32,73 @@ class TestStaffAnimal:
 
         # Assert that console output is NULL
         assert console_output.strip() == ""
+
+
+# Test the 'Staff' class
+class TestStaff:
+
+    # :param capsys: pytest argument. Allows console output to be captured.
+    def test_read(self, capsys):
+        # Arrange
+        staff = Staff()
+
+        # Act
+        staff.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        # Assert that console output is not NULL
+        assert console_output.strip() != ""
+
+        # :param capsys: pytest argument. Allows console output to be captured.
+
+    def test_add(self, capsys):
+        # Arrange
+        staff = Staff()
+        new_staff = ("Courtney Boy", 2)
+        animal_ids = (1, 19, 30)
+
+        # Get output before add
+        staff.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        old_output_length = len(console_output.splitlines())
+
+        # Act
+        staff.add_staff(new_staff, animal_ids)
+
+        # Get output after add
+        staff.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        new_output_length = len(console_output.splitlines())
+
+        # Assert
+        assert new_output_length > old_output_length
+
+    def test_delete(self, capsys):
+        # Arrange
+        staff = Staff()
+        staff_id = 1
+
+        # Get output before delete
+        staff.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        old_output_length = len(console_output.splitlines())
+
+        # Act
+        staff.delete_staff(staff_id)
+
+        # Get output after add
+        staff.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        new_output_length = len(console_output.splitlines())
+
+        # Assert
+        assert new_output_length < old_output_length
