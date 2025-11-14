361944993142c9e501304924ed9ecbe7b98ab980
lfcourtney <lukechopper22@gmail.com>
2025-11-14 19:40:05 +0000

    Added TestAnimal class

--- test_database.py
+++ test_database.py
@@ -1,5 +1,5 @@
 import pytest
-from challenge import StaffAnimal, Staff
+from challenge import StaffAnimal, Staff, Animal
 
 
 # Test the 'StaffAnimal' class
@@ -102,3 +102,75 @@ class TestStaff:
 
         # Assert
         assert new_output_length < old_output_length
+
+
+# Test the 'Animal' class
+class TestAnimal:
+
+    # :param capsys: pytest argument. Allows console output to be captured.
+    def test_read(self, capsys):
+        # Arrange
+        animal = Animal()
+
+        # Act
+        animal.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        # Assert that console output is not NULL
+        assert console_output.strip() != ""
+
+        # :param capsys: pytest argument. Allows console output to be captured.
+
+    def test_add(self, capsys):
+        # Arrange
+        animal = Animal()
+        animal_type_id = 1
+        zoo_section_id = 1
+
+        new_animal = (animal_type_id, zoo_section_id)
+
+        # Get output before add
+        animal.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        old_output_length = len(console_output.splitlines())
+
+        # Act
+        animal.add_animal(new_animal)
+
+        # Get output after add
+        animal.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        new_output_length = len(console_output.splitlines())
+
+        # Assert
+        assert new_output_length > old_output_length
+
+    def test_delete(self, capsys):
+        # Arrange
+        animal = Animal()
+        animal_id = 1
+
+        # Get output before delete
+        animal.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        old_output_length = len(console_output.splitlines())
+
+        # Act
+        animal.delete_animal(animal_id)
+
+        # Get output after add
+        animal.select_all()
+        captured = capsys.readouterr()
+        console_output = captured.out
+
+        new_output_length = len(console_output.splitlines())
+
+        # Assert
+        assert old_output_length < new_output_length
